ba7b214b78592394a20e4fa9dd5b28cb8f70e9b6
Aelita <aelita.le-rolland@outlook.fr>
2024-01-24 15:27:31 +0100

    Initial script to Tiles for BFS algo

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : TacticsMove
+{
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
