2eca5c81183e6b0065049302662f2328fac7bc91
Aelita Le ROlland <arthur.lerolland@live.fr>
2024-02-02 11:42:02 +0100

    Make the character move on the path

--- Assets/Scripts/MassageCardiaque/Character.cs
+++ Assets/Scripts/MassageCardiaque/Character.cs
@@ -5,18 +5,40 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     [SerializeField]
-    private float _yOffset = 1.25f; 
+    private float _yOffset = 1.25f;
     private Cell _target;
     [SerializeField]
     private Cell _currentCell;
 
+    [SerializeField]
+    private float _moveSpeed;
+
+    public List<Cell> path = new List<Cell>();
+    public bool isMoving = false;
+    private Cell _nextCell;
+
     public int movePoints = 4;
+
+
+    private void Update()
+    {
+        if (isMoving)
+        {
+            MoveToNextCell();
+        }
+    }
+
+    public void Reset()
+    {
+        _target = null;
+        path.Clear();
+    }
 
     public void Acte()
     {
         if (_target != null)
         {
-            Move(_target);
+            Move();
             _target.SetState(Cell.CellState.Idle);
         }
     }
@@ -27,11 +49,40 @@ public class Character : MonoBehaviour
         cell.SetState(Cell.CellState.isSelected);
     }
 
-    public void Move(Cell target)
+    private void MoveToNextCell()
     {
-         Vector3 destination = new Vector3(target.transform.position.x, _yOffset, target.transform.position.z);
-         transform.position = destination;
-        SetCurrentCell(target);
+        Vector3 target = new Vector3(_nextCell.transform.position.x, _nextCell.transform.position.y + _yOffset, _nextCell.transform.position.z);
+        if (Vector3.Distance(transform.position, target) >= 0.05f)
+        {
+            //Rendre le mouvement smooth
+
+            transform.position = target;
+
+            //Sa marche pas, jsp pk:
+            //Vector3.MoveTowards(transform.position, target, _moveSpeed /** Time.deltaTime*/);
+        }
+        else if(_nextCell == _target)
+        {
+            isMoving = false;
+        }
+        else
+        {
+            path.Remove(_nextCell);
+            SetCurrentCell(_nextCell);
+            _nextCell = path[0];
+        }
+    }
+
+    public void Move()
+    {
+        if (_target != null)
+        {
+            isMoving = true;
+            _nextCell = path[0];
+        }
+         //Vector3 destination = new Vector3(target.transform.position.x, _yOffset, target.transform.position.z);
+         //transform.position = destination;
+         //SetCurrentCell(target);
     }
 
     public void SetCurrentCell(Cell cell)
