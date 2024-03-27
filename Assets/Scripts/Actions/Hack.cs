4627133e9af42ea847d0c1d1c0593577d5e86fc7
Aelita Le ROlland <arthur.lerolland@live.fr>
2024-03-27 18:08:18 +0100

    Works for hacking

--- Assets/Scripts/Actions/Hack.cs
+++ Assets/Scripts/Actions/Hack.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class Hack : CellAction
 {
+    float _actionDelay = 0.5f;
+    float _remainDelay;
+
+    bool _isActing = false;
+
     protected override void Awake()
     {
         base.Awake();
         action = Actions.HACK;
+        _remainDelay = _actionDelay;
     }
+
+    private void Update()
+    {
+        if (_isActing)
+        {
+            _remainDelay -= Time.deltaTime;
+            if (_remainDelay <= 0) ContinueActe();
+        }
+    }
+
     public override bool Acte(int characterStat, PlayerCharacter character)
     {
         if (Random.Range(0f,1f) < character.hackStunChance)
@@ -20,12 +36,27 @@ public class Hack : CellAction
         if (_cell.remainDifficulty <= 0)
         {
             EventsManager.instance.RaiseSFXEvent(SFX_Name.CAMERA_CONNEXION);
-            HackAllCams();
+            _isActing=true;
+
 
             return true;
         }
         return false;
     }
+
+    private void ContinueActe()
+    {
+        _isActing = false;
+
+        HackAllCams();
+
+        foreach (Cell cell in _cell.linkCell)
+        {
+            HackAllCams();
+        }
+
+    }
+
 
     private void HackAllCams()
     {
